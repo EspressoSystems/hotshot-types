eed0be279f7efcfe8a9139738ec83727eed4636c
Brendon Fish <bfish713@gmail.com>
2024-03-14 15:57:24 -0400

    add view sync timeout to config

--- src/utils.rs
+++ src/utils.rs
@@ -171,6 +171,7 @@ impl AsRef<Sha256Digest> for BuilderCommitment {
 ///   - Litte endian encoding
 ///   - Varint encoding
 ///   - Reject trailing bytes
+#[allow(clippy::type_complexity)]
 #[must_use]
 pub fn bincode_opts() -> WithOtherTrailing<
     WithOtherIntEncoding<
