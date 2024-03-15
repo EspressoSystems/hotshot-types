f75c8da63d461685e6b7c5370a58bfdc3f345a25
Jarred Parr <jparr721@gmail.com>
2024-03-15 09:07:22 -0400

    restrict type

--- src/traits/block_storage.rs
+++ src/traits/block_storage.rs
@@ -31,6 +31,6 @@ pub enum ProposalType<TYPES: NodeType> {
 
 /// Abstraction for storing the contents of DA proposals and VID shares.
 #[async_trait]
-pub trait BlockStorage<TYPES: NodeType> {
+pub trait BlockStorage<TYPES: NodeType>: Send + Sync + Clone {
     async fn append(&self, proposal: &ProposalType<TYPES>) -> Result<(), BlockStorageError>;
 }
