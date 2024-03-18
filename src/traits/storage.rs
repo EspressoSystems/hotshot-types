36edde92ee46fdd44393812d048c0bb51acdd90d
Brendon Fish <bfish713@gmail.com>
2024-03-18 16:17:00 -0400

    return result for high_qc

--- src/traits/storage.rs
+++ src/traits/storage.rs
@@ -21,5 +21,5 @@ pub trait Storage<TYPES: NodeType>: Send + Sync + Clone {
     async fn append_vid(&self, proposal: &Proposal<TYPES, VidDisperse<TYPES>>) -> Result<()>;
     async fn append_da(&self, proposal: &Proposal<TYPES, DAProposal<TYPES>>) -> Result<()>;
     async fn record_action(&self, view: TYPES::Time, action: HotShotAction) -> Result<()>;
-    async fn update_high_qc(&self, qc: QuorumCertificate<TYPES>);
+    async fn update_high_qc(&self, qc: QuorumCertificate<TYPES>) -> Result<()>;
 }
