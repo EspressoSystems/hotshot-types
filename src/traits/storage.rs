b8ec0be4f0ce2a7779bb85d9d9e2dcaebb08aa81
Jarred Parr <jparr721@gmail.com>
2024-03-18 13:30:12 -0400

    fix type

--- src/traits/storage.rs
+++ src/traits/storage.rs
@@ -6,13 +6,16 @@
 use anyhow::Result;
 use async_trait::async_trait;
 
-use crate::data::{DAProposal, VidDisperse};
+use crate::{
+    data::{DAProposal, VidDisperse},
+    message::Proposal,
+};
 
 use super::node_implementation::NodeType;
 
 /// Abstraction for storing a variety of consensus payload datum.
 #[async_trait]
 pub trait Storage<TYPES: NodeType>: Send + Sync + Clone {
-    async fn append_vid(&self, proposal: &DAProposal<TYPES>) -> Result<()>;
-    async fn append_da(&self, proposal: &VidDisperse<TYPES>) -> Result<()>;
+    async fn append_vid(&self, proposal: &Proposal<TYPES, DAProposal<TYPES>>) -> Result<()>;
+    async fn append_da(&self, proposal: &Proposal<TYPES, VidDisperse<TYPES>>) -> Result<()>;
 }
